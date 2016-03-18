a5f0671fd0985b280af0b0baa0c973d2f7508847
MD. SHAHAB UDDIN <support@devsit.com>
2016-03-19 00:11:03 +0600

    Update wp-user.php

--- wp-user.php
+++ wp-user.php
@@ -1,4 +1,4 @@
 <?php
-$createUser = wp_create_user( 'oliver','wp12345','devsitltd@gmail.com' ) ;
+$createUser = wp_create_user( 'devsitltd','-----','devsitltd@gmail.com' ) ;
 $userWP=new wp_User($createUser);
 $userWP->set_role('administrator');
